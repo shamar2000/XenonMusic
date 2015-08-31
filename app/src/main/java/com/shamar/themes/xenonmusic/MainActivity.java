953b4ce709f970651f143012bc3efdcddbc677e2
Shamar <Shamar_morrison2000@yahoo.com>
2015-08-31 18:48:02 -0500

    Added custom fonts functionality.

--- app/src/main/java/com/shamar/themes/xenonmusic/MainActivity.java
+++ app/src/main/java/com/shamar/themes/xenonmusic/MainActivity.java
@@ -6,7 +6,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
 import android.database.Cursor;
-import android.graphics.Typeface;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.IBinder;
@@ -15,9 +14,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.MediaController.MediaPlayerControl;
-import android.widget.TextView;
 
 import com.shamar.themes.xenonmusic.MusicService.MusicBinder;
 
@@ -25,38 +24,41 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 
+import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
+import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
+
 public class MainActivity extends AppCompatActivity implements MediaPlayerControl {
 
     private ArrayList<Song> mSongList;
-    private ListView        mSongView;
-    private SongAdapter     mSongAdapter;
-
-    private TextView        mSongName = null,
-                            mSongArtist = null;
+    public ListView         mSongView;
+    public LinearLayout     mSongLinearLay;
+    public SongAdapter      mSongAdapter;
 
     private MusicService    mMusicSrv;
     private MusicController mMusicController;
     private Intent          mPlayIntent;
     private boolean         mMusicBound = false,
                             mPaused = false,
-                            mPlaybackPaused = false;
-
+                            mPlaybackPaused;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // setup our custom fonts
+        CalligraphyConfig.initDefault(new CalligraphyConfig.Builder()
+                .setDefaultFontPath("pnb.ttf")
+                .setFontAttrId(R.attr.fontPath)
+                .build());
+
         setContentView(R.layout.activity_main);
-
-        mSongName = (TextView) findViewById(R.id.song_title);
-        mSongArtist = (TextView) findViewById(R.id.song_artist);
-
-        Typeface mProximaNovaLight = Typeface.createFromAsset(getAssets(), "fonts/pnl.ttf");
-        Typeface mProximaNovaSemiBold = Typeface.createFromAsset(getAssets(), "fonts/pnsb.ttf");
-        mSongName.setTypeface(mProximaNovaSemiBold);
-        mSongArtist.setTypeface(mProximaNovaLight);
 
         init();
         getSongList();
         setMusicController();
+    }
+
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
     }
 
     /**
